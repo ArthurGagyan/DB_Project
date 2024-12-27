f72409da1dc3a2f4377acb2547f2620348e186eb
ArthurGagyan <147807192+ArthurGagyan@users.noreply.github.com>
2024-12-27 19:39:34 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import items
+from app.database import engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(items.router)
